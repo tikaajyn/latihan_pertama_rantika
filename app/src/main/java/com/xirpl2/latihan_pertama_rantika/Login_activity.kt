a812734c385656bfd2ffbbc9855117950f88a8af
Siti Irma <irmaeryani08@gmail.com>
2023-10-20 15:52:12 +0700

    Rantika XI rpl 2

---
+++ app/src/main/java/com/xirpl2/latihan_pertama_rantika/Login_activity.kt
@@ -0,0 +1,11 @@
+package com.xirpl2.latihan_pertama_rantika
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Login_activity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
